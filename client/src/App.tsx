6826b11144ed7abd0ca029b53f3fd5b6999a6a9b
Shweta Wankhade <shweta.wankhade21@vit.edu>
2025-04-04 16:21:36 +0530

    error handling in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -7,6 +8,7 @@ import Home from "@/pages/home";
 import Analysis from "@/pages/analysis";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 function Router() {
   return (
@@ -21,15 +23,16 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-       <ThemeProvider>
-       <div className="flex flex-col min-h-screen bg-background">
-        <NavBar />
-        <div className="flex-grow">
-          <Router />
+      <ThemeProvider>
+        <div className="flex flex-col min-h-screen bg-background">
+          <NavBar />
+          <div className="flex-grow">
+            <Router />
+          </div>
+          <Footer />
+          <Toaster />
         </div>
-        <Footer />
-      </div>
-      <Toaster />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
